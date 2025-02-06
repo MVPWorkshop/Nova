20a040751aff36a90fb76fbde6b389fa279fb6f1
Milos Djurica <milos.djurica@mvpworkshop.co>
2025-02-06 13:35:37 +0100

    pedersen ck field public

--- src/provider/pedersen.rs
+++ src/provider/pedersen.rs
@@ -23,7 +23,8 @@ pub struct CommitmentKey<E: Engine>
 where
   E::GE: DlogGroup,
 {
-  ck: Vec<<E::GE as DlogGroup>::AffineGroupElement>,
+  /// TODO
+  pub ck: Vec<<E::GE as DlogGroup>::AffineGroupElement>,
   h: Option<<E::GE as DlogGroup>::AffineGroupElement>,
 }
 
